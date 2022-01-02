76519481089bd2a48a6228711d6cf152e945030c
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2022-01-02 13:49:18 +0100

    add \'blank\' button

--- src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManagerWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManagerWindow.java
@@ -1,18 +1,13 @@
 package edu.kis.powp.jobs2d.command.gui;
 
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-
 import edu.kis.powp.appbase.gui.WindowComponent;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
 import edu.kis.powp.observer.Subscriber;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.List;
 
 public class CommandManagerWindow extends JFrame implements WindowComponent {
 
@@ -24,7 +19,7 @@ public class CommandManagerWindow extends JFrame implements WindowComponent {
 	private JTextArea observerListField;
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 9204679248304669948L;
 
@@ -63,6 +58,14 @@ public class CommandManagerWindow extends JFrame implements WindowComponent {
 		c.gridx = 0;
 		c.weighty = 1;
 		content.add(btnClearCommand, c);
+		//new button
+		JButton bntGiveCommand = new JButton("Give command");
+		bntGiveCommand.addActionListener((ActionEvent e) -> this.giveCommand());
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1;
+		c.gridx = 0;
+		c.weighty = 1;
+		content.add(bntGiveCommand, c);
 
 		JButton btnClearObservers = new JButton("Delete observers");
 		btnClearObservers.addActionListener((ActionEvent e) -> this.deleteObservers());
@@ -75,6 +78,14 @@ public class CommandManagerWindow extends JFrame implements WindowComponent {
 
 	private void clearCommand() {
 		commandManager.clearCurrentCommand();
+		updateCurrentCommandField();
+	}
+
+	//new command
+	private void giveCommand() {
+		//implement here something
+			
+//		commandManager.clearCurrentCommand();
 		updateCurrentCommandField();
 	}
 
