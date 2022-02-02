5ea69d4cfa2ba48690c2573d218f47164b81766f
wioleta1501 <wioleta1501@gmail.com>
2022-02-02 20:02:44 +0100

    extensions fixed

--- src/main/java/edu/kis/powp/jobs2d/drivers/SelectDriverMenuOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/SelectDriverMenuOptionListener.java
@@ -2,10 +2,8 @@ package edu.kis.powp.jobs2d.drivers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.drivers.composite.DriverComposite;
 import edu.kis.powp.jobs2d.features.ExtensionFeature;
 
 public class SelectDriverMenuOptionListener implements ActionListener {
@@ -21,15 +19,6 @@ public class SelectDriverMenuOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		List<Job2dDriver> extensions =  extensionsManager.getActiveExtensionList();
-		if(extensions.isEmpty()){
-			driverManager.setCurrentDriver(driver);
-		} else {
-			DriverComposite driverComposite = new DriverComposite(driverManager.getCurrentDriver());
-			for (Job2dDriver driver: extensions) {
-				driverComposite.add(driver);
-			}
-			driverManager.setCurrentDriver(driverComposite);
-		}
+		driverManager.setCurrentDriver(driver);
 	}
 }
